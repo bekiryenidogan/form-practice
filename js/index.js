6849c6d9cc66b0baa38f5bc41e1e2296f5a352b0
Bekir Yenidogan <bekiryenidogan16@gmail.com>
2022-09-30 19:25:15 +0300

    Sign-Up Form Example

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+  const pass1 = document.querySelector('#pass').value;
+  const pass2 = document.querySelector('#confirm-pass').value;
+  if (pass1 !== pass2) {
+    alert("Passwords doesnt match");
+    return ;
+  }
+   if (pass1.length <8) {
+   alert("Password must be atleast 8 characters");
+   return;
+    }
+
+alert("Yay!")
+});
